c411ce8d9f3457effbb0e38b8d6cbba08826a472
chosorio <christian.h.osorio@gmail.com>
2015-01-06 18:11:42 -0800

    A36417 Version 0.0.1
    Continuing setup. Set up TRIS for the registers. Next steps are to set
    up the scaling factors for the inputs.

--- A36417-000.X/A36417.c
+++ A36417-000.X/A36417.c
@@ -13,8 +13,7 @@ void DoStateMachine(void);
 void InitializeA36417(void);
 void DoA36417_000(void);
 
-MCP4822 U42_MCP4822;
-MCP4822 U44_MCP4822;
+MCP4822 U11_MCP4822;
 
 
 unsigned int control_state;
@@ -53,12 +52,146 @@ void DoStateMachine(void){
 
 void DoA36417_000(void){
 
+if(_T5IF){
+
+    _T5IF=0;
+
+ // -------------------- CHECK FOR FAULTS ------------------- //
+
+
+    if (global_reset_faults) {
+      etm_can_system_debug_data.debug_0++;
+      etm_can_status_register.status_word_1 = 0x0000;
+      global_reset_faults = 0;
+    }
+
+
+}
 
     return;
 }
 
 void InitializeA36417(void){
 
-    
-    return;
+    U11_MCP4822.pin_chip_select_not = _PIN_RF2;
+    U11_MCP4822.pin_load_dac_not = _PIN_RF3;
+    U11_MCP4822.spi_port = ETM_SPI_PORT_1;
+    U11_MCP4822.spi_con1_value = MCP4822_SPI_CON_VALUE;
+    U11_MCP4822.spi_con2_value = MCP4822_SPI_CON2_VALUE;
+    U11_MCP4822.spi_stat_value = MCP4822_SPI_STAT_VALUE;
+    U11_MCP4822.spi_bit_rate = MCP4822_SPI_1_M_BIT;
+    U11_MCP4822.fcy_clk = FCY_CLK;
+
+    SetupMCP4822(&U11_MCP4822);
+
+
+  // Configure Inhibit Interrupt
+  _INT3IP = 7; // This must be the highest priority interrupt
+  _INT1EP = 0; // Positive Transition
+
+  // Configure ADC Interrupt
+  _ADIP   = 6; // This needs to be higher priority than the CAN interrupt (Which defaults to 4)
+
+
+  // Initialize all I/O Registers
+  TRISA = A36417_TRISA_VALUE;
+  TRISB = A36417_TRISB_VALUE;
+  TRISC = A36417_TRISC_VALUE;
+  TRISD = A36417_TRISD_VALUE;
+  TRISF = A36417_TRISF_VALUE;
+  TRISG = A36417_TRISG_VALUE;
+
+   // Initialize TMR5
+  T5CON = T5CON_VALUE;
+  TMR5  = 0;
+  _T5IF = 0;
+  PR5   = PR5_VALUE_10_MILLISECONDS;
+
+  // Initialize integral ADC
+  // ---- Configure the dsPIC ADC Module ------------ //
+  ADCON1 = ADCON1_SETTING;             // Configure the high speed ADC module based on H file parameters
+  ADCON2 = ADCON2_SETTING;             // Configure the high speed ADC module based on H file parameters
+  ADCON3 = ADCON3_SETTING;             // Configure the high speed ADC module based on H file parameters
+  ADCHS  = ADCHS_SETTING;              // Configure the high speed ADC module based on H file parameters
+
+  ADPCFG = ADPCFG_SETTING;             // Set which pins are analog and which are digital I/O
+  ADCSSL = ADCSSL_SETTING;             // Set which analog pins are scanned
+  _ADIF = 0;
+  _ADIE = 1;
+  _ADON = 1;
+
+   // Initialize the CAN module
+  ETMCanInitialize();
+
+  // Flash LEDs at boot up
+  __delay32(1000000);
+  ClrWdt();
+  PIN_LED_OPERATIONAL = 1;
+
+  __delay32(1000000);
+  ClrWdt();
+  PIN_LED_TEST_POINT_A = 1;
+
+  __delay32(1000000);
+  ClrWdt();
+  PIN_LED_TEST_POINT_B = 1;
+
+
+
+}
+
+
+void __attribute__((interrupt, no_auto_psv)) _ADCInterrupt(void) {
+  _ADIF = 0;
+
+  if (_BUFS) {
+      // read ADCBUF 0-7
+      global_data_A36417_000.analog_input_ion_pump_current.adc_accumulator          += ADCBUF0;
+      global_data_A36417_000.analog_input_ion_pump_voltage.adc_accumulator          += ADCBUF1;
+      global_data_A36417_000.analog_input_5V_monitor.adc_accumulator                +=ADCBUF2;
+      global_data_A36417_000.analog_input_15V_monitor.adc_accumulator               +=ADCBUF3;
+      global_data_A36417_000.analog_input_minus_5V_monitor.adc_accumulator          +=ADCBUF4;
+      global_data_A36417_000.analog_input_target_current.adc_accumulator            +=ADCBUF5;
+    } else {
+      // read ADCBUF 8-15
+      global_data_A36417_000.analog_input_ion_pump_current.adc_accumulator          += ADCBUF8;
+      global_data_A36417_000.analog_input_ion_pump_voltage.adc_accumulator          += ADCBUF9;
+      global_data_A36417_000.analog_input_5V_monitor.adc_accumulator                +=ADCBUFA;
+      global_data_A36417_000.analog_input_15V_monitor.adc_accumulator               +=ADCBUFB;
+      global_data_A36417_000.analog_input_minus_5V_monitor.adc_accumulator          +=ADCBUFC;
+      global_data_A36417_000.analog_input_target_current.adc_accumulator            +=ADCBUFD;
+    }
+
+  global_data_A36417_000.accumulator_counter += 1;
+
+  if (global_data_A36417_000.accumulator_counter >= 128) {
+
+      global_data_A36417_000.analog_input_ion_pump_current.adc_accumulator >>= 3;  // This is now a 16 bit number average of previous 128 samples
+      global_data_A36417_000.analog_input_ion_pump_current.filtered_adc_reading = global_data_A36417_000.analog_input_ion_pump_current.adc_accumulator;
+      global_data_A36417_000.analog_input_ion_pump_current.adc_accumulator = 0;
+
+      global_data_A36417_000.analog_input_ion_pump_voltage.adc_accumulator >>= 3;  // This is now a 16 bit number average of previous 128 samples
+      global_data_A36417_000.analog_input_ion_pump_voltage.filtered_adc_reading = global_data_A36417_000.analog_input_ion_pump_voltage.adc_accumulator;
+      global_data_A36417_000.analog_input_ion_pump_voltage.adc_accumulator = 0;
+
+      global_data_A36417_000.analog_input_target_current.adc_accumulator >>= 3;  // This is now a 16 bit number average of previous 128 samples
+      global_data_A36417_000.analog_input_target_current.filtered_adc_reading = global_data_A36417_000.analog_input_target_current.adc_accumulator;
+      global_data_A36417_000.analog_input_target_current.adc_accumulator = 0;
+
+      global_data_A36417_000.analog_input_5V_monitor.adc_accumulator >>= 3;  // This is now a 16 bit number average of previous 128 samples
+      global_data_A36417_000.analog_input_5V_monitor.filtered_adc_reading = global_data_A36417_000.analog_input_5V_monitor.adc_accumulator;
+      global_data_A36417_000.analog_input_5V_monitor.adc_accumulator = 0;
+
+      global_data_A36417_000.analog_input_15V_monitor.adc_accumulator >>= 3;  // This is now a 16 bit number average of previous 128 samples
+      global_data_A36417_000.analog_input_15V_monitor.filtered_adc_reading = global_data_A36417_000.analog_input_15V_monitor.adc_accumulator;
+      global_data_A36417_000.analog_input_15V_monitor.adc_accumulator = 0;
+
+      global_data_A36417_000.analog_input_minus_5V_monitor.adc_accumulator >>= 3;  // This is now a 16 bit number average of previous 128 samples
+      global_data_A36417_000.analog_input_minus_5V_monitor.filtered_adc_reading = global_data_A36417_000.analog_input_minus_5V_monitor.adc_accumulator;
+      global_data_A36417_000.analog_input_minus_5V_monitor.adc_accumulator = 0;
+
+      global_data_A36417_000.accumulator_counter = 0;
+  }
+
+
 }
--- A36417-000.X/A36417.h
+++ A36417-000.X/A36417.h
@@ -17,13 +17,171 @@
 #include "ETM_CAN_PUBLIC.h"
 #include "ETM_ANALOG.h"
 
+
+
 typedef struct{
     AnalogInput analog_input_ion_pump_current;
     AnalogInput analog_input_ion_pump_voltage;
     AnalogInput analog_input_target_current;
-    
+    AnalogInput analog_input_5V_monitor;
+    AnalogInput analog_input_15V_monitor;
+    AnalogInput analog_input_minus_5V_monitor;
     AnalogOutput analog_output_emco_control;
+
+    unsigned int accumulator_counter;
+
 }IonPumpControlData;
+
+// ------------------------ CONFIGURE ADC MODULE ------------------- //
+
+
+// -------- Analog Input Pins ----------//
+
+
+
+#define PIN_A_IN_ION_PUMP_VOLTAGE               _RB3
+#define PIN_A_IN_ION_PUMP_CURRENT               _RB4
+#define PIN_A_IN_5V_MONITOR                     _RB5
+#define PIN_A_IN_15V_MONITOR                    _RB6
+#define PIN_A_IN_MINUS_5V_MONITOR               _RB7
+
+#define PIN_A_IN_TARGET_CURRENT                 _RB10
+#define PIN_A_IN_SPARE_1                        _RB11
+#define PIN_A_IN_SPARE_2                        _RB12
+
+/*
+  This sets up the ADC to work as following
+  AUTO Sampling
+  VDD / GND as reference
+
+  With 10MHz System Clock, ADC Clock is 450ns, Sample Time is 4 ADC Clock so total sample time is 9uS
+  Conversion rate of 111KHz (27.8 Khz per Channel), 277 Samples per 10mS interrupt
+
+  8 Samples per Interrupt, use alternating buffers
+
+
+*/
+
+#define ADCON1_SETTING          (ADC_MODULE_OFF & ADC_IDLE_STOP & ADC_FORMAT_INTG & ADC_CLK_AUTO & ADC_AUTO_SAMPLING_ON)
+#define ADCON2_SETTING          (ADC_VREF_AVDD_AVSS & ADC_SCAN_ON & ADC_SAMPLES_PER_INT_8 & ADC_ALT_BUF_ON & ADC_ALT_INPUT_OFF)
+#define ADCON3_SETTING          (ADC_SAMPLE_TIME_4 & ADC_CONV_CLK_SYSTEM & ADC_CONV_CLK_9Tcy2)
+#define ADCHS_SETTING           (ADC_CH0_POS_SAMPLEA_AN2 & ADC_CH0_NEG_SAMPLEA_VREFN & ADC_CH0_POS_SAMPLEB_AN2 & ADC_CH0_NEG_SAMPLEB_VREFN) //Why AN3? Shouldn't it just be a ground or negative rail?
+#define ADPCFG_SETTING          (ENABLE_AN3_ANA & ENABLE_AN4_ANA & ENABLE_AN5_ANA & ENABLE_AN6_ANA & ENABLE_AN7_ANA & ENABLE_AN10_ANA & ENABLE_AN11_ANA & ENABLE_AN12_ANA)
+#define ADCSSL_SETTING          (SKIP_SCAN_AN0 & SKIP_SCAN_AN1 & SKIP_SCAN_AN2 & SKIP_SCAN_AN8 & SKIP_SCAN_AN9 & SKIP_SCAN_AN13 & SKIP_SCAN_AN14 &SKIP_SCAN_AN15) //Modified to skip unused inputs
+
+
+
+/*
+   TMR5 Configuration
+   Timer5 - Used for 10msTicToc
+   Period should be set to 10mS
+   With 10Mhz Clock, x8 multiplier will yield max period of 17.7mS, 2.71uS per tick
+*/
+
+#define T5CON_VALUE                    (T5_ON & T5_IDLE_CON & T5_GATE_OFF & T5_PS_1_8 & T5_SOURCE_INT)
+#define PR5_PERIOD_US                  10000   // 10mS
+#define PR5_VALUE_10_MILLISECONDS      (FCY_CLK_MHZ*PR5_PERIOD_US/8)
+
+// -------------------- A36417_000 FAULTS/WARNINGS CONFIGURATION-------------------- //
+#define FAULT_BIT_ION_PUMP_OVER_CURRENT             FAULT_BIT_USER_DEFINED_1
+#define FAULT_BIT_ION_PUMP_OVER_VOLTAGE             FAULT_BIT_USER_DEFINED_2
+#define FAULT_BIT_ION_PUMP_UNDER_VOLTAGE            FAULT_BIT_USER_DEFINED_3
+
+// -------------------- A36417_000 STATUS BIT CONFIGURATION ------------------------ //
+
+
+
+// -------------------- A36417_000 FAULTS/WARNINGS CONFIGURATION-------------------- //
+#define FAULT_BIT_ION_PUMP_OVER_CURRENT         FAULT_BIT_USER_DEFINED_1
+#define FAULT_BIT_ION_PUMP_OVER_VOLTAGE         FAULT_BIT_USER_DEFINED_2
+#define FAULT_BIT_ION_PUMP_UNDER_VOLTAGE        FAULT_BIT_USER_DEFINED_3
+
+/*
+
+-----------Digital Input Pins------------
+
+RA14 - Digital Input - Sample Target Current
+*/
+
+#define PIN_D_IN_SAMPLE_TARGET_CURRENT  _RA14
+
+/*
+
+-----------Digital Output Pins-------------
+
+RA7 - Digital Output - LED Operational
+RA8 - Digital Output - Test Point E
+RA12 - Digital Output - Test Point C
+RA13 - Digital Output - Test Point D
+
+RB2 - Digital Output - Test Point F
+RB8 - Digital Output - Test Point G
+RB9 - Digital Output - Test Point H
+
+RG12 - Digital Output - Led Test Point A
+RG13 - Digital Output - Led Test Point B
+RG14 - Digital Output - Reset detect
+*/
+
+#define PIN_LED_OPERATIONAL       _LATA7
+#define PIN_LED_TEST_POINT_A      _LATG12
+#define PIN_LED_TEST_POINT_B      _LATG13
+
+#define PIN_D_OUT_TEST_POINT_E      _LATA6
+
+#define PIN_D_OUT_TEST_POINT_C      _LATA12
+#define PIN_D_OUT_TEST_POINT_D      _LATA13
+#define PIN_D_OUT_TEST_POINT_F      _LATB2
+#define PIN_D_OUT_TEST_POINT_G      _LATB8
+#define PIN_D_OUT_TEST_POINT_H      _LATB9
+
+#define PIN_D_OUT_RESET_DETECT      _LATG14
+
+/*
+-------------Analog Inputs-------------
+
+RB3 - Analog Input - Ion pump voltage
+RB4 - Analog Input - Ion pump current
+RB5 - Analog Input - 5V monitor
+RB6 - Analog Input - -5V monitor
+RB7 - Analog Input - -15v monitor
+
+RB10 - Analog Input - Target Current
+*/
+
+
+/*
+-----------------Not used---------------
+
+RB11
+RB12
+
+--------- Pins that are overidden by a hardware module and should be left as inputs during port configuration ----
+RB0 - PROGRAM
+RB1 - PROGRAM
+
+RF0 - CAN 1
+RF1 - CAN 1
+RF6 - SPI 1
+RF8 - SPI 1
+
+-------- Pins that are configured by other software modules and should be left as inputs during port configuration -----------
+
+RF2 (DAC CS/LS)
+RF3  (DAC LDAC)
+
+*/
+
+#define A36417_TRISA_VALUE 0b1100000000000000
+#define A36417_TRISB_VALUE 0b0000010011111011
+#define A36417_TRISC_VALUE 0b0000000000000000
+#define A36417_TRISD_VALUE 0b0000000000000000
+#define A36417_TRISF_VALUE 0b0000000101001111
+#define A36417_TRISG_VALUE 0b0000000000000000
+
+#define ION_PUMP_CURRENT_SCALE_FACTOR              1 //1V per 1uA
+#define ION_PUMP_VOLTAGE_SCALE_FACTOR              .501//1V per 1kV
+
 
 
 #endif	/* A36417_H */
